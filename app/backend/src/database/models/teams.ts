57265193fe98c030cd2705bb3c8354d1bccde92e
Luiz Guilherme <guilhermerx18@gmail.com>
2022-08-18 16:04:50 -0300

    feat: add model and migration for teams

---
+++ app/backend/src/database/models/teams.ts
@@ -0,0 +1,28 @@
+import { INTEGER, Model, STRING } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  id!: number;
+  teamName!: string;
+}
+
+Team.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  timestamps: false,
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+});
+
+export default Team;
